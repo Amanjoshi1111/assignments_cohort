b04646a216688bf2f1b9f83f7b486144e40d5ade
Aman Joshi <amanjoshi@Amans-MacBook-Air.local>
2023-12-22 23:23:53 +0530

    merge conflict resolved

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -1,6 +1,7 @@
 /*
  * Write a function that halts the JS thread (make it busy wait) for a given number of milliseconds.
  * During this time the thread should not be able to do anything else.
+ * the function should return a promise just like before
  */
 
 function sleep(n) {
@@ -16,3 +17,7 @@ function sleep(n) {
 
 sleep(2);
 console.log("after");
+function sleep(milliseconds) {
+}
+
+module.exports = sleep;
